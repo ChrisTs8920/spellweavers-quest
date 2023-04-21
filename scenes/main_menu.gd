c78206300d8771b46c0bdafdac7c8138eb6fcf64
Chris <ChrisT8920@gmail.com>
2023-04-21 14:04:53 +0300

    Rework parallax background

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,6 +13,19 @@ func _ready():
 func _process(delta):
 	var vec = get_viewport().get_mouse_position()
 	$PointLight2D.position = vec # move by that vector
+	
+	var mouse_x = vec.x
+	var mouse_y = vec.y
+	var relative_x = (mouse_x - (get_viewport_transform().x.x/2)) / (get_viewport_transform().x.x/2)
+	var relative_y = (mouse_y - (get_viewport_transform().y.y/2)) / (get_viewport_transform().y.y/2)
+	$ParallaxBackground/ParallaxLayer2.motion_offset.x = .01 * relative_x
+	$ParallaxBackground/ParallaxLayer2.motion_offset.y = .01 * relative_y
+	
+	$ParallaxBackground/ParallaxLayer3.motion_offset.x = .02 * relative_x
+	$ParallaxBackground/ParallaxLayer3.motion_offset.y = .02 * relative_y
+	
+	$ParallaxBackground/ParallaxLayer4.motion_offset.x = .04 * relative_x
+	$ParallaxBackground/ParallaxLayer4.motion_offset.y = .04 * relative_y
 
 
 
